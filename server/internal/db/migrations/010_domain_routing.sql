14c0d9b105e8ed18411c79d5ab6af1147be31e5c
jamal <jamsakino404@gmail.com>
2026-04-06 14:24:30 +0100

    Fix migration to use IF NOT EXISTS for idempotent reruns

--- server/internal/db/migrations/010_domain_routing.sql
+++ server/internal/db/migrations/010_domain_routing.sql
@@ -1,7 +1,7 @@
 -- +goose Up
-ALTER TABLE domains ADD COLUMN target_type TEXT NOT NULL DEFAULT '';
-ALTER TABLE domains ADD COLUMN target_subdomain TEXT NOT NULL DEFAULT '';
-CREATE INDEX idx_domains_verified_lookup ON domains(domain) WHERE verified = true AND target_subdomain != '';
+ALTER TABLE domains ADD COLUMN IF NOT EXISTS target_type TEXT NOT NULL DEFAULT '';
+ALTER TABLE domains ADD COLUMN IF NOT EXISTS target_subdomain TEXT NOT NULL DEFAULT '';
+CREATE INDEX IF NOT EXISTS idx_domains_verified_lookup ON domains(domain) WHERE verified = true AND target_subdomain != '';
 
 -- +goose Down
 DROP INDEX IF EXISTS idx_domains_verified_lookup;
